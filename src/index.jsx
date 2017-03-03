0c20555745a71b61872e3fcb61179a3d362952c8
Ashish <ashish240284@gmail.com>
2017-03-03 19:54:16 +0530

    server setting with --hot flag in start line.

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { AppContainer } from 'react-hot-loader'
 export default class Home extends React.Component{
     constructor(props){
         super(props);
@@ -10,6 +10,8 @@ export default class Home extends React.Component{
             <div>
             <h1>Hello World</h1>
             <p>My first project in ReactJS</p>
+                <h2>Nothing is difficult</h2>
+                <h4>fghhhdhhgj</h4>
             </div>
         );
     }
