16e7d3de3299af5b610a63e028fa09b45d88d4c5
Ashish <ashish240284@gmail.com>
2017-03-04 17:12:43 +0530

    final commit on routing

---
+++ src/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Home extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render() {
+        return(
+            <div>
+               <h2>This is Home Page</h2>
+                <p>im enoying routing on reactJS</p>
+            </div>
+        );
+    }
+}
